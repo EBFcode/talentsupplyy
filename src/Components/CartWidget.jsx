bb4932570551bb3ce9811cc48dfb4e9735fe93c5
imcod3r <codekickebf@gmail.com>
2022-06-23 23:40:48 -0400

    agregandoEstilos

--- src/Components/CartWidget.jsx
+++ src/Components/CartWidget.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './CartWidget.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
 
 export default function CartWidget() {
     return (
         <div className="Widget">
-            <FontAwesomeIcon icon={['fab', 'apple']} />
+            <FontAwesomeIcon icon="fa-solid fa-cart-shopping-fast" />
             <p>0</p>
         </div>
     )
--- src/Components/ItemListContainer.jsx
+++ src/Components/ItemListContainer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import './ItemListContainer.css'
+
 
 export default function ItemListContainer({greeting}) {
     return (
-        <div>
-            {greeting}
+        <div className="containerGreeting">
+            
+            <h2>{greeting}</h2>
+            
         </div>
     )
 }
