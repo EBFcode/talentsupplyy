2f50aa7c9a4bb8d1a2a02e00b773863b16d445f3
imcod3r <codekickebf@gmail.com>
2022-06-24 11:32:38 -0400

    AgregandoDisenoBoton

--- src/Components/ItemCount.jsx
+++ src/Components/ItemCount.jsx
@@ -4,7 +4,19 @@ import React from 'react'
 export default function ItemCount() {
     return (
         <div className="containerCount">
-            <button>Sumar</button>
+            <div className="CountObjets">
+                <div className="textContainer">
+                    <h3>Agrega Columnas donde escribir texto</h3>
+                </div>
+
+                <div className="CountObjet">
+                    <button>-</button>
+                    <h4>1</h4>
+                    <button>+</button>
+                </div>
+            </div>
+            
+
         </div>
     )
 }
