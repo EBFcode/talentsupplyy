4d194156320017b21ee7f5fda352ba663ac27c22
imcod3r <codekickebf@gmail.com>
2022-06-24 17:36:11 -0400

    Evitar incrementar mas que el Stock, animacion de no incrementar mas.

--- src/Components/ItemCount.jsx
+++ src/Components/ItemCount.jsx
@@ -7,15 +7,40 @@ export default function ItemCount({maxStock}) {
     // Declarando un STATE
     const [disponibleStock, setDisponibleStock] = useState(maxStock);
 
+    let btnMas = document.getElementById("mas");
+    let btnMenos = document.getElementById("menos");
+    let textStock = document.getElementById("textStock")
+
+
+
     function onAdd(){
-        setNumero(numero + 1);
-        setDisponibleStock(disponibleStock - 1);
+
+        if (numero <= (maxStock - 1)){
+            setNumero(numero + 1);
+            setDisponibleStock(disponibleStock - 1);
+            btnMenos.style.backgroundColor = "#2B4162";
+
+
+        }else {
+            btnMas.style.backgroundColor = "#f2f2f2";
+            textStock.style.color = "red";
+        }
         
     }
 
-    function restaSeleccion(){
-        setNumero(numero - 1);
-        setDisponibleStock(disponibleStock + 1);
+    function onSubtract(){
+
+        if (disponibleStock < maxStock) {
+            setNumero(numero - 1);
+            setDisponibleStock(disponibleStock + 1);
+            btnMas.style.backgroundColor = "#2B4162";
+            textStock.style.color = "#fff";
+
+
+        }else {
+            btnMenos.style.backgroundColor = "#f2f2f2";
+        }
+        
     }
     return (
         <div className="containerCount">
@@ -23,11 +48,11 @@ export default function ItemCount({maxStock}) {
             <div className="CountObjets">
 
                 <div className="CountObjet">
-                    <h4>Stock Disponible: {disponibleStock}</h4>
+                    <h4 id="textStock">Stock Disponible: {disponibleStock}</h4>
                 </div>
 
                 <div className="CountObjet">
-                    <button onClick={() => restaSeleccion()} id="menos">-</button>
+                    <button onClick={() => onSubtract()} id="menos">-</button>
                     <h4>{numero}</h4>
                     {/* Funcion sumar 1 */}
                     <button onClick={() => onAdd()} id="mas">+</button>
