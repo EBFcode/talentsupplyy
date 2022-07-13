17397c65de0316cd0702e107bf8633157e31d60c
EBF <ebfvirtual@gmail.com>
2022-07-12 21:43:22 -0400

    Pasamos de Promise a Fetch
    con la ayuda de:
    Co-authored-by: Maxi Rebolo <MaxiR23@users.noreply.github.com>

--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -3,7 +3,6 @@ import './ItemListContainer.css'
 import ItemList from './itemList'
 import { useParams } from 'react-router-dom'
 
-
 export default function ItemListContainer() {
     const [loading, setLoading] = React.useState(true)
     const [items, setItems] = React.useState([])
@@ -11,41 +10,20 @@ export default function ItemListContainer() {
 
     React.useEffect(() => {
 
-        const prod = [
-            {
-                id: 1,
-                title: 'samsung',
-                price: 1000,
-                pictureUrl: "https://images.samsung.com/is/image/samsung/assets/latin/2202/pcd/smp/PCD_B_KV_Merchandising_264x264_mo.png?$264_264_PNG$",
-                stock: 5
-            },
-            {
-                id: 2,
-                title: 'apple',
-                price: 2500,
-                pictureUrl: "https://www.att.com/idpassets/global/devices/phones/apple/apple-iphone-12/carousel/blue/64gb/6861C-1_carousel.png",
-                stock: 7
-            }
-        ]
+        let url = 'https://imcod3r.github.io/CatStoreApi/api.json'
 
-        let task = new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(prod)
-            }, 2000)
-        })
-
-        task
-        .then(response => {
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
             if (!idCategory) {
-                setItems(response)
+                setItems(data);
             } else {
-                const arrayFiltered = response.filter((item) => item.title === idCategory);
-                setItems(arrayFiltered)
-                console.log('hola', arrayFiltered)
+            const arrayFiltrado = data.filter((item) => item.categoria === idCategory);
+            setItems(arrayFiltrado)
             }
         })
-        .catch(err => console.log(err))
-        .finally(() => setLoading(false));
+        .catch(error => console.warn(error))
+        .finally(() => setLoading(false))
 
     }, [idCategory])
 
