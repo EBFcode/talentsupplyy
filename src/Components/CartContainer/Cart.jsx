cd90b4d93e4da0b8a9ab1b92e3df76d5e32cfe58
EBF <ebfvirtual@gmail.com>
2022-08-10 19:43:27 -0400

    agregado CARRITO VACIO

--- src/Components/CartContainer/Cart.jsx
+++ src/Components/CartContainer/Cart.jsx
@@ -9,6 +9,9 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { CartContext } from '../../Context/CartContext';
 import { useNavigate } from 'react-router-dom';
+import Typography from '@mui/material/Typography'
+import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
+import { Button } from '@mui/material';
 
 export default function CartWidget() {
 
@@ -16,44 +19,59 @@ export default function CartWidget() {
   const navigate = useNavigate()
 
   return (
-    <Container fixed>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 400 }} aria-label="spanning table">
-          <TableHead sx={{backgroundColor: '#000'}}>
-            <TableRow sx={{backgroundColor: '#161032'}}>
-              <TableCell align="center" sx={{ color: 'white' }} colSpan={3}>
-                Detalles
-              </TableCell>
-              <TableCell align="right" sx={{ color: 'white' }} >Precio</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell sx={{ color: 'white' }}>Descripcion</TableCell>
-              <TableCell sx={{ color: 'white' }} align="right">Cantidad.</TableCell>
-              <TableCell sx={{ color: 'white' }} align="right">Unidad</TableCell>
-              <TableCell sx={{ color: 'white' }} align="right">Total</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody >
-            {cart.map((row, i) => (
-              <TableRow key={i}>
-                <TableCell >{row.nombre}</TableCell>
-                <TableCell align="right">{row.quantity}</TableCell>
-                <TableCell align="right">{row.precio}</TableCell>
-                <TableCell align="right">{row.precio * row.quantity}</TableCell>
-              </TableRow>
-            ))}
+    <>
+      {cart.length === 0 ? 
+        <Container fixed>
+            <Typography variant="h3" color="initial" align='center' sx={{ p: 5 }}>
+              CARRITO VACIÓ
+              <br />
+              <ProductionQuantityLimitsIcon />
+              <br />
+              <Button href='/' variant="outlined">IR A HOME</Button>
+            </Typography>
+        </Container>
+      : 
+        <Container fixed>
+              <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 400 }} aria-label="spanning table">
+                  <TableHead sx={{backgroundColor: '#000'}}>
+                    <TableRow sx={{backgroundColor: '#161032'}}>
+                      <TableCell align="center" sx={{ color: 'white' }} colSpan={3}>
+                        Detalles
+                      </TableCell>
+                      <TableCell align="right" sx={{ color: 'white' }} >Precio</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell sx={{ color: 'white' }}>Descripcion</TableCell>
+                      <TableCell sx={{ color: 'white' }} align="right">Cantidad.</TableCell>
+                      <TableCell sx={{ color: 'white' }} align="right">Unidad</TableCell>
+                      <TableCell sx={{ color: 'white' }} align="right">Total</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody >
+                    {cart.map((row, i) => (
+                      <TableRow key={i}>
+                        <TableCell >{row.nombre}</TableCell>
+                        <TableCell align="right">{row.quantity}</TableCell>
+                        <TableCell align="right">{row.precio}</TableCell>
+                        <TableCell align="right">{row.precio * row.quantity}</TableCell>
+                      </TableRow>
+                    ))}
 
-            <TableRow>
-              <TableCell rowSpan={3} />
-              <TableCell colSpan={2}>Total</TableCell>
-              <TableCell align="right">{total}</TableCell>
-              <TableCell align="right"><button onClick={() => navigate('/checkout')}>Pagar</button></TableCell>
-            </TableRow>
-            
-          </TableBody>
-        </Table>
-      </TableContainer>
+                    <TableRow>
+                      <TableCell rowSpan={3} />
+                      <TableCell colSpan={2}>Total</TableCell>
+                      <TableCell align="right">{total}</TableCell>
+                      <TableCell align="right"><button onClick={() => navigate('/checkout')}>Pagar</button></TableCell>
+                    </TableRow>
+                    
+                  </TableBody>
+                </Table>
+              </TableContainer>
 
-    </Container>
+            </Container>
+          }
+    </>
+    
   );
 }
